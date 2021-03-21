208fb920ee011dad7dd449a9a7f9666724f98ddc
kirandash <thekirankumardash@gmail.com>
2021-03-21 20:09:32 +0800

    4.3 Reducer Setup

---
+++ 4.3 Reducer Setup/src/state/reducers/repositoriesReducer.ts
@@ -0,0 +1,24 @@
+// 4.3 Reducer Setup
+
+// state interface
+interface RepositoriesState {
+  loading: boolean;
+  error: string | null;
+  data: string[];
+}
+
+// reducer
+const reducer = (state: RepositoriesState, action: any) => {
+  switch (action.type) {
+    case 'search_repositories':
+      return { loading: true, error: null, data: [] };
+    case 'search_repositories_success':
+      return { loading: false, error: null, data: action.payload };
+    case 'search_repositories_error':
+      return { loading: false, error: action.payload, data: [] };
+    default:
+      return state;
+  }
+};
+
+export default reducer;
