f0a946bfc57af1ae1d8f4870096c4197690b2ff3
kirandash <thekirankumardash@gmail.com>
2021-03-21 20:32:19 +0800

    4.6. Typing an Action

--- 4.3 Reducer Setup/src/state/reducers/repositoriesReducer.ts
+++ 4.3 Reducer Setup/src/state/reducers/repositoriesReducer.ts
@@ -7,9 +7,15 @@ interface RepositoriesState {
   data: string[];
 }
 
+// 4.6. Typing an Action
+interface Action {
+  type: string;
+  payload?: any; // optional
+}
+
 // reducer
 // 4.5. Annotating the Return Type for reducer with RepositoriesState
-const reducer = (state: RepositoriesState, action: any): RepositoriesState => {
+const reducer = (state: RepositoriesState, action: Action): RepositoriesState => {
   switch (action.type) {
     case 'search_repositories':
       return { loading: true, error: null, data: [] };
