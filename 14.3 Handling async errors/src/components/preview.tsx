ae09b170fe70e619c4c3f0b444ff942e2a3bac2a
kirandash <thekirankumardash@gmail.com>
2021-03-21 15:44:52 +0800

    14.3 Handling async errors - preview.tsx

---
+++ 14.3 Handling async errors/src/components/preview.tsx
@@ -0,0 +1,65 @@
+import './preview.css';
+import { useRef, useEffect } from 'react';
+
+interface PreviewProps {
+  code: string;
+}
+
+const html = `
+    <html>
+      <head>
+        <style>html { background-color: white; }</style>
+      </head>
+      <body>
+        <div id="root"></div>
+        <script>
+          // Extracting reusable error logic
+          const handleError = (err) => {
+            const root = document.querySelector('#root');
+            root.innerHTML = '<div style="color: red;"><h4>Runtime Error</h4>' + err + '</div>';
+            console.error(err);
+          };
+
+          // Handling async or uncaught errors
+          window.addEventListener('error', (event) => {
+            // by default uncaught errors are logged in console. to prevent, use event.preventDefault()
+            event.preventDefault();
+            handleError(event.error);
+          });
+          
+          // catch run time errors
+          window.addEventListener('message', (event) => {
+            try {
+              eval(event.data);
+            } catch (err) {
+              handleError(err);
+            }
+          }, false);
+        </script>
+      </body>
+    </html>
+  `;
+
+const Preview: React.FC<PreviewProps> = ({ code }) => {
+  const iframe = useRef<any>();
+
+  useEffect(() => {
+    iframe.current.srcdoc = html;
+    setTimeout(() => {
+      iframe.current.contentWindow.postMessage(code, '*');
+    }, 50);
+  }, [code]);
+
+  return (
+    <div className="preview-wrapper">
+      <iframe
+        title="preview"
+        ref={iframe}
+        sandbox="allow-scripts"
+        srcDoc={html}
+      />
+    </div>
+  );
+};
+
+export default Preview;
